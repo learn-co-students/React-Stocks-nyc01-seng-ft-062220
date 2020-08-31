51cc3c2f983a86a61d658126c94799d0ca1b6ff5
dukhyunko93 <dukhyunko93@gmail.com>
2020-08-30 21:02:01 -0400

    Done.

--- src/components/Stock.js
+++ src/components/Stock.js
@@ -1,21 +1,24 @@
 import React from 'react'
 
-const Stock = () => (
-  <div>
+const Stock = (props) => {
 
+  let addStock = () => {
+    props.clickHandler(props.stock)
+  }
+
+  let removeStock = () => {
+    props.stock.remove = true
+    props.clickHandler(props.stock)
+  }
+
+  return(
     <div className="card">
-      <div className="card-body">
-        <h5 className="card-title">{
-            //Company Name
-          }</h5>
-        <p className="card-text">{
-            //ticker: stock price
-          }</p>
+      <div className="card-body" onClick={props.portfolio ? removeStock : addStock}>
+        <h5 className="card-title">{props.stock.name}</h5>
+        <p className="card-text">{props.stock.ticker}: {props.stock.price}</p>
       </div>
     </div>
-
-
-  </div>
-);
+  )  
+};
 
 export default Stock
--- src/containers/PortfolioContainer.js
+++ src/containers/PortfolioContainer.js
@@ -2,14 +2,16 @@ import React, { Component } from 'react';
 import Stock from '../components/Stock'
 
 class PortfolioContainer extends Component {
+  
+  renderStocks = () => {
+    return this.props.myStockArray.map(stockObj => <Stock key={stockObj.id} stock={stockObj} clickHandler={this.props.clickHandler} portfolio />)
+  }
 
   render() {
     return (
       <div>
         <h2>My Portfolio</h2>
-          {
-            //render your portfolio stocks here
-          }
+          { this.renderStocks() }
       </div>
     );
   }
