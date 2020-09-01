41366eb51e47e88558a4c8abc6cf208f6132933c
vakas-786 <vakas0959@gmail.com>
2020-09-01 08:48:59 -0400

    Done.

--- src/containers/PortfolioContainer.js
+++ src/containers/PortfolioContainer.js
@@ -3,12 +3,18 @@ import Stock from '../components/Stock'
 
 class PortfolioContainer extends Component {
 
+clickHandler = (event) => {
+  
+  this.props.deleteStock(event)
+}
+
   render() {
+    let renderPortfolio = this.props.portfolio.map((stockObj) => <Stock key={stockObj.id} stock={stockObj} clickHandler={this.clickHandler}/>)
     return (
       <div>
         <h2>My Portfolio</h2>
           {
-            //render your portfolio stocks here
+           renderPortfolio
           }
       </div>
     );
